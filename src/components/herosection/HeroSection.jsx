b8d48724a36a8eaad172df516a4402d133a94752
vigneshvaranasi <yeswaraditya3006@gmail.com>
2024-09-24 21:08:18 +0530

    feat:Finsh Hero Section

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import EswarImgCenter from '../../assets/hero-section/eswarimg.png'
 
 function HeroSection() {
     return (
-        <div>HeroSection</div>
+        <div className='mt-10 mb-10 '>
+            <div className='flex justify-between'>
+                <div className='flex-1 sm:me-20'></div>
+                <div className='fontGimletNarrow sm:ms-20 flex flex-col sm:flex-row justify-center'>
+                    <img src={EswarImgCenter} width={250} alt="" />
+                    <div className='sm:ms-3 italic font-light'>
+                        <p className='text-2xl'>UI/UX Designer</p>
+                        <p className='text-2xl'>Developer</p>
+                    </div>
+                </div>
+                <div className='flex-1'></div>
+            </div>
+
+            <div className='fontGimletNarrow text-center mt-5'>
+                <h1 className='text-7xl font-normal'>Eswar Aditya*</h1>
+                <p className='mt-4 text-2xl font-light'>U r not Lazy, just CREATIVE</p>
+            </div>
+        </div>
+
+
+
+
+
+        // <div className='mt-10'>
+        //     <div className='fontGimletNarrow flex flex-col sm:flex-row justify-center'>
+        //         <img src={EswarImgCenter} width={250} alt="" />
+        //         <div>
+        //             <p className='text-2xl'>UI/UX Designer</p>
+        //             <p className='text-2xl'>Developer</p>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,13 +7,13 @@ function Navbar() {
     <div className='flex flex-col sm:flex-row justify-between items-center px-6 mt-6'>
       {/*Logo*/}
       <div className='flex-1'>
-        <h1 className='text-4xl'>YEA.</h1>
+        <h1 className='text-4xl fontGimletCompressed italic font-light'>YEA.</h1>
       </div>
       {/*Dynamic Island*/}
       <div className='flex-2 mt-4 sm:mt-0 flex justify-center'>
         <div className='flex items-center bg-black text-white rounded-full px-6 sm:px-8 py-2 sm:py-1.5'>
           <img src={eswarImage} width={25} alt="Eswar" className='mr-2 sm:mr-5' />
-          <p className='text-sm font-semibold'>Try what makes you sleepless</p>
+          <p className='text-sm font-semibold '>Try what makes you sleepless</p>
           <img src={Music} width={25} alt="Music" className='ml-2 sm:ml-5' />
         </div>
       </div>
